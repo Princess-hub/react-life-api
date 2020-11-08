e0f80a00dfc4f9d50d7ff917688f56bb5addb3c3
Princess <plinda.mensah@gmail.com>
2020-11-08 17:45:25 +0000

    react life api

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Users from './Users';
+import UsersAgain from './UsersAgain';
+
+function App() {
+  return (
+    // <Users />
+    <UsersAgain />
+  );
+}
+
+export default App;
